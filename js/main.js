0fa289d264e90fcfa5d210d2f5b4ab6be3968269
Brown <c3055151@hallam.shu.ac.uk>
2023-11-17 12:15:49 +0000

    Sixth Commit

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@
         document.querySelector("nav ul li").setAttribute("class", "redBack");
     }
 
-    document.querySelector(".red").addEventListener("click", function(ev){ //passing the event object to the event handler function
+    /*document.querySelector(".red").addEventListener("click", function(ev){ //passing the event object to the event handler function
         document.querySelector("body").setAttribute("class", "redBack");
     })
 
@@ -53,8 +53,22 @@
 
     document.querySelector(".reset").addEventListener("click", function(ev){
         document.querySelector("body").removeAttribute("class"); //resets page background colour
-        document.querySelector("nav ul li").removeAttribute("class");
-    })
+        document.querySelector("nav ul li").removeAttribute("class"); //resets home nav link colour
+    })*/
+
+    //the below code is better and more optimised than the above scripts
+    let colourButtons = document.querySelectorAll(".colPicker"); //creating a variable called colourButtons which is the same as the .colPicker class shown in html eg. green colPicker, blue colPicker etc
+    let bodyElement = document.querySelector("Body"); //this creates a bodyElement variable which is the same as the body in html so it's within the body tags  
+    console.dir(colourButtons); //this shows which elements it is finding and shows within console
+
+    for(let i=0; i < colourButtons.length; i++){ //creating a loop which runs for the amount of times as there are colourButtons - uses indexes from 0 upwards so 7 elements will cause it to check from 0 to 6
+        colourButtons[i].addEventListener("click", chgColour) //this adds an event listener to all of the colourButtons so that when they are clicked it will run the chgColour function below
+    }
+
+    function chgColour(ev){ //chgColour function passing on the ev (events) and using that within the function
+        let colourPicked = ev.target.classList[0] + "Back"; //finds the classlist of "Back" which is in the mobile.css file eg. .redBack, .greenBack etc and sets the colourPicked variable to the same as the "Back" classes
+        bodyElement.setAttribute("class", colourPicked) //sets the bodyElement which is the body - background as the same colour as colourPicked
+    }
 
 
     //end code here
